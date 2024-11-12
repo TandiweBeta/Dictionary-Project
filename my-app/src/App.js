31e37e37049fbee1a847396eaad21b781999e23f
TandiweBeta <tandiwebeta@gmail.com>
2024-11-12 13:25:56 +0200

    added the title

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,12 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary App</header>
+      <header className="App-header">
+        <h1>Dictionary App </h1>
+        <a href="#" className="btn btn-primary">
+          Search
+        </a>
+      </header>
     </div>
   );
 }
